e6a66404a39317f50283a38593e0a30efbad4a26
Yuan-Qian-XD <yuan.qian@alumnos.upm.es>
2021-05-13 18:28:37 +0200

    Version Alfa 1.1
    Parte principal del mapa movimiento.
    Estructura principal

--- myLib.c
+++ myLib.c
@@ -1,3 +1,64 @@
+#define N 15
+#define M 3//NIVELES
+int level=0;
+int x, y;
+
+int mapa[M][N][N]={
+    {
+    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+    {0,3,1,1,1,0,1,1,1,0,0,0,0,0,0},
+    {0,1,0,0,1,0,0,0,1,0,1,1,1,0,0},
+    {0,1,1,1,1,0,1,1,1,0,1,0,1,0,0},
+    {0,0,1,0,1,0,1,0,1,1,1,0,1,0,0},
+    {0,1,1,0,1,0,1,0,1,0,0,0,1,1,0},
+    {0,1,0,0,1,1,1,0,1,1,1,0,0,1,0},
+    {0,1,1,0,1,1,0,0,0,0,1,0,0,0,0},
+    {0,0,0,1,1,0,1,0,0,0,1,1,1,0,0},
+    {0,1,0,1,1,1,0,0,0,0,0,0,1,0,0},
+    {0,1,0,1,0,1,1,0,0,0,0,0,1,1,4},
+    {0,0,1,1,0,1,1,1,0,1,0,1,0,1,0},
+    {0,1,0,1,0,0,0,1,0,1,1,0,1,0,0},
+    {0,1,0,1,1,1,1,1,1,1,1,0,1,1,0},
+    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+
+},
+{
+    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+    {0,3,1,0,1,1,1,1,1,0,0,0,0,0,0},
+    {0,1,0,0,1,0,0,0,1,1,1,1,1,0,0},
+    {0,1,1,1,1,1,1,1,0,0,1,0,1,0,0},
+    {0,0,0,0,1,0,1,0,1,1,1,0,1,0,0},
+    {0,1,0,0,1,0,1,0,1,0,0,0,1,1,0},
+    {0,1,0,0,1,1,1,0,1,1,1,0,0,1,0},
+    {0,1,1,0,1,1,0,0,0,0,1,0,0,0,0},
+    {0,0,0,1,1,0,1,0,0,0,1,0,1,0,0},
+    {0,1,1,1,1,1,0,0,1,1,1,0,1,0,0},
+    {0,1,0,1,0,1,1,0,1,0,0,0,1,1,0},
+    {0,0,1,1,0,1,1,0,1,1,1,1,0,1,0},
+    {0,1,0,1,0,0,0,1,0,0,1,0,1,0,0},
+    {0,1,0,1,1,1,1,1,1,0,1,0,1,1,0},
+    {0,0,0,0,0,0,0,0,0,0,4,0,0,0,0},
+},
+{
+    {0,0,0,0,0,0,0,0,0,0,0,0,0,4,0},
+    {0,3,1,1,0,1,0,0,0,1,1,1,0,1,0},
+    {0,0,1,1,1,1,0,0,0,1,0,1,0,1,0},
+    {0,0,1,1,0,1,0,0,0,1,0,1,1,1,0},
+    {0,0,1,0,0,1,0,0,1,1,0,0,0,0,0},
+    {0,0,1,1,0,1,0,0,1,1,0,1,0,0,0},
+    {0,0,0,1,0,0,0,0,0,1,0,1,1,1,0},
+    {0,1,1,1,1,1,1,1,1,1,0,0,0,1,0},
+    {0,1,0,0,0,0,1,0,0,0,0,1,0,1,0},
+    {0,1,1,0,0,0,0,0,1,0,0,1,0,1,0},
+    {0,1,1,1,1,1,1,1,1,1,1,1,1,1,0},
+    {0,0,1,0,0,1,0,0,0,0,0,1,0,0,0},
+    {0,0,1,1,1,1,1,0,0,0,0,1,0,0,0},
+    {0,0,1,0,0,1,0,0,0,0,0,1,1,1,0},
+    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+},
+};
+
+
 void menu_principal() //Para regresar al menu principal
 {
     char seleccion;
@@ -40,7 +101,6 @@ void iniciar_juego()
         scanf("%i",&elige);
         char apodo[21];
 
-
         if(elige==1)
         {
             limpiar();
@@ -58,17 +118,19 @@ void iniciar_juego()
             iniciar_juego();
         }
     printf("\n Buienvenido, guerrero %s\n Aqui comienza tu historia......\n\n\n",apodo);
-printf("A la espera del contenido.........................................................................");
     system("pause");
-///////JUEGO/////JUEGO/////JUEGO/////JUEGO/////JUEGO/////JUEGO/////JUEGO/////JUEGO/////JUEGO/////JUEGO
-//JUEGO/////JUEGO/////JUEGO/////JUEGO/////JUEGO/////JUEGO/////JUEGO/////JUEGO/////JUEGO/////JUEGO/////
-///////JUEGO/////JUEGO/////JUEGO/////JUEGO/////JUEGO/////JUEGO/////JUEGO/////JUEGO/////JUEGO/////JUEGO
-//JUEGO/////JUEGO/////JUEGO/////JUEGO/////JUEGO/////JUEGO/////JUEGO/////JUEGO/////JUEGO/////JUEGO/////
-///////JUEGO/////JUEGO/////JUEGO/////JUEGO/////JUEGO/////JUEGO/////JUEGO/////JUEGO/////JUEGO/////JUEGO
-//JUEGO/////JUEGO/////JUEGO/////JUEGO/////JUEGO/////JUEGO/////JUEGO/////JUEGO/////JUEGO/////JUEGO/////
-///////JUEGO/////JUEGO/////JUEGO/////JUEGO/////JUEGO/////JUEGO/////JUEGO/////JUEGO/////JUEGO/////JUEGO
-//JUEGO/////JUEGO/////JUEGO/////JUEGO/////JUEGO/////JUEGO/////JUEGO/////JUEGO/////JUEGO/////JUEGO/////
-    salir();
+
+//Entra al menu de juego
+    int flag=0;
+
+    menu_juego();
+
+    while(1)
+    {
+        flag= movimiento();
+        if(flag==1||flag==-1)
+            return 0;
+    }
     return 0;
 }
 
@@ -91,4 +153,173 @@ return 0;
 void limpiar()
 {
    system("cls||clear");
+}
+
+void menu_juego()
+{
+    limpiar();
+    char select [10];
+    printf("\nDiga el nivel(1,2,3):\n");
+    while(1) //ELEGIR LOS NIVELES
+    {
+         scanf("%c", select);
+
+             if('1'==select[0])
+             {
+                 level=0;
+                 break;
+             }
+             else if('2'==select[0])
+             {
+                 level=1;
+                 break;
+             }
+             else if('3'==select[0])
+             {
+                 level=2;
+                 break;
+             }
+             else
+             {
+                 printf("Escoja el nivel deseado:\n");
+             }
+
+         }
+}
+
+ void posicion()//POSICION DEL PERSONAJE
+{
+    int i=0, j=0;
+    for(i=0;i<N;i++)
+    {
+        for(j=0;j<N; j++)
+        {
+        if(mapa[level][i][j]==3)
+               {
+                   x=i;
+                   y=j;
+                   break;
+               }
+        }
+    }
+}
+
+int movimiento()
+{
+    int flag = 0;
+    while(1)
+    {
+        flag=0;
+        posicion();
+        print();
+        switch(getch())//RECIBIR ORDENES
+        {
+            case 'w': flag=Up();
+            break;
+            case 'a': flag=Left();
+            break;
+            case 's': flag=Down();
+            break;
+            case 'd': flag=Right();
+            break;
+        }
+        if(flag==1)
+        {
+            if(level==M-1)
+            {
+                printf("Finalizado el juego\n");
+                return 1;//TERMINAR LA PARTIDA
+            }
+        else
+        {
+            system("pause");
+            level++;//SIGUIENTE NIVEL
+            return 0;
+        }}
+    }
+}
+int Up()
+{
+    if(x!=0)
+    {
+        if(mapa[level][x-1][y]==1)//funciona
+        {
+            mapa[level][x-1][y]=3;
+            mapa[level][x][y]=1;
+        }
+        else if(mapa[level][x-1][y]==4)
+        {
+            return 1;
+        }
+    }
+    return 0;
+}
+int Down()
+{
+    if(x!=N-1)
+    {
+        if(mapa[level][x+1][y]==1)//funciona
+        {
+            mapa[level][x+1][y]=3;
+            mapa[level][x][y]=1;
+        }
+        else if(mapa[level][x+1][y]==4)
+        {
+            return 1;
+        }
+    }
+    return 0;
+}
+int Right()
+{
+if(y!=N-1)
+    {
+        if(mapa[level][x][y+1]==1)//funciona
+        {
+            mapa[level][x][y+1]=3;
+            mapa[level][x][y]=1;
+        }
+        else if(mapa[level][x][y+1]==4)
+        {
+            return 1;
+        }
+    }
+    return 0;
+}
+int Left()
+{
+if(x!=0)
+    {
+        if(mapa[level][x][y-1]==1)//funciona
+        {
+            mapa[level][x][y-1]=3;
+            mapa[level][x][y]=1;
+        }
+        else if(mapa[level][x][y-1]==4)
+        {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+void print() //Imprimir el mapa en pantalla
+{
+    int i=0, j=0;
+    limpiar();
+    for(i=0;i<N;i++)
+    {
+        for(j=0;j<N; j++)
+        {
+            if(mapa[level][i][j]==0)
+                printf("�");//PARED
+            else if(mapa[level][i][j]==3)
+                printf("O");//PERSONAJE
+            else if(mapa[level][i][j]==1)
+                printf(" ");//ESPACIO
+            else if(mapa[level][i][j]==4)
+                printf("X");//SALIDA
+        }
+        printf("\n");
+    }
 }
--- myLib.h
+++ myLib.h
@@ -1,6 +1,16 @@
 void menu_principal();
+void menu_juego();
 void iniciar_juego();
 void instrucciones();
 void salir();
 void limpiar();
 char apodo[21];
+void print();
+void posicion();
+
+//Aqui comienza funciones del movimiento
+int movimiento();
+int Up();
+int Down();
+int Left();
+int Right();
